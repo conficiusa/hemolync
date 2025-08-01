e57c4eb816b8b452a0b0da2e82f03f9a17692d26
Conficius <addawebadua@gmail.com>
2025-08-01 10:19:57 +0000

    log server url

--- src/lib/server/api.ts
+++ src/lib/server/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL 
+const API_URL = process.env.NEXT_PUBLIC_API_URL 
+console.log('API URL:', API_URL)
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
