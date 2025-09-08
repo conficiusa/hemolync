f97c41b93630cfab92ed18f9e2df95da6c9a50e8
conficiusa <addawebadua@gmail.com>
2025-09-08 23:49:33 +0000

    Refactor code structure for improved readability and maintainability.

--- src/routes/dashboard/(card-routes)/route.tsx
+++ src/routes/dashboard/(card-routes)/route.tsx
@@ -1,3 +1,4 @@
+import { Layers, Send, Tags } from 'lucide-react'
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 import { StatCard } from '@/components/stat-card'
 
@@ -10,22 +11,25 @@ function OverviewCardsLayout() {
     <main>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <StatCard
-          title="Total Blood In Stock"
+          icon={Layers}
+          title="Total Units In Stock"
           value="200 Units"
           change={8.5}
           changeType="increase"
           changeText="Up from yesterday"
         />
         <StatCard
-          title="Total Transferred"
-          value="80 Units"
+          icon={Send}
+          title="Total units sent"
+          value="80 units"
           change={4}
           changeType="decrease"
           changeText="Down from yesterday"
           changeColor="destructive"
         />
         <StatCard
-          title="Total Request"
+          icon={Tags}
+          title="Total units received"
           value="50 Units"
           change={10}
           changeType="increase"
