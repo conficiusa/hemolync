c40a841d82ed0a5fa540a461cf86dc9439755a19
Conficius <116452045+conficiusa@users.noreply.github.com>
2025-10-03 08:30:59 +0000

    Update add-product.schema.ts

--- src/lib/schemas/product-schemas/add-product.schema.ts
+++ src/lib/schemas/product-schemas/add-product.schema.ts
@@ -5,6 +5,5 @@ export const addBloodSchema = z.object({
   blood_type: z.string().min(1, 'Blood group is required'),
   blood_product: z.string().min(1, 'Please specify type of blood product'),
   expiry_date: z.string().min(1, 'expiration date of blood is required'),
-  added_by: z.string().min(1, 'Who is adding this blood'),
   quantity: z.number().gt(0, 'Quantity must be greater than 0'),
 })
